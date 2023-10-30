809da1fa2f5035b06b39ecbe14e63313b3a4b57e
sangkeenam <144409636+sangkeenam@users.noreply.github.com>
2023-10-30 14:07:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def main():
                 for symbol in stock_codes:
                     # Yahoo Finance에서 주식 정보를 가져옵니다.
                     stock = yf.Ticker(symbol)
+                    time.sleep(5)
                     # 기업 정보를 출력합니다.
                     st.write("---")
                     #st.write("stock.info all:", stock)
